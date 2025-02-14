1d3ea163768aa70a70e79b8ffda76c76f5e1f22f
Jae Hyuk (Joseph) Lee <jhl011@ucsd.edu>
2025-02-14 12:24:18 +0900

    Cleaned and Refined the Code

--- bbox.cuh
+++ bbox.cuh
@@ -37,21 +37,22 @@ __host__ __device__ inline HitResult Hit(const BBox &bbox, Ray ray) {
     float3 t0 = (bbox.p_min - ray.org) * inv_dir;
     float3 t1 = (bbox.p_max - ray.org) * inv_dir;
 
-if (inv_dir.x < 0.0f) {
-    float temp = t0.x;
-    t0.x = t1.x;
-    t1.x = temp;
-}
-if (inv_dir.y < 0.0f) {
-    float temp = t0.y;
-    t0.y = t1.y;
-    t1.y = temp;
-}
-if (inv_dir.z < 0.0f) {
-    float temp = t0.z;
-    t0.z = t1.z;
-    t1.z = temp;
-}
+    if (inv_dir.x < 0.0f) {
+        float temp = t0.x;
+        t0.x = t1.x;
+        t1.x = temp;
+    }
+
+    if (inv_dir.y < 0.0f) {
+        float temp = t0.y;
+        t0.y = t1.y;
+        t1.y = temp;
+    }
+    if (inv_dir.z < 0.0f) {
+        float temp = t0.z;
+        t0.z = t1.z;
+        t1.z = temp;
+    }
 
     float tnear = max(max(t0.x, t0.y), t0.z);
     float tfar = min(min(t1.x, t1.y), t1.z);
@@ -71,16 +72,19 @@ inline __host__ __device__ bool hit(const BBox &bbox, Ray ray) {
         t0.x = t1.x;
         t1.x = temp;
     }
+
     if (inv_dir.y < 0.0f) {
         float temp = t0.y;
         t0.y = t1.y;
         t1.y = temp;
     }
+
     if (inv_dir.z < 0.0f) {
         float temp = t0.z;
         t0.z = t1.z;
         t1.z = temp;
     }
+
     ray.tnear = max(max(t0.x, t0.y), t0.z);
     ray.tfar = min(min(t1.x, t1.y), t1.z);
     return ray.tfar >= max(0.0f, ray.tnear);
